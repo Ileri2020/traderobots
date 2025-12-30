836d0f5ab90897b4d239c4fb41232b665814fcc4
Adepoju Ololade Joshua Ilerioluwa <adepojuololade2020@gmail.com>
2025-12-30 15:25:13 +0100

    Switch to deterministic Vercel Builds+Routes and rename application to app

--- api/index.py
+++ api/index.py
@@ -9,4 +9,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'traderobots.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
