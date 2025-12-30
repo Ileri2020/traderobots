82940d1904b4dd4a3fe5ff29a48d2054bcb6b117
Adepoju Ololade Joshua Ilerioluwa <adepojuololade2020@gmail.com>
2025-12-30 14:54:59 +0100

    Refactor to Vercel Zero Config: Move API to root api/ folder

---
+++ api/index.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+# Add the backend directory to sys.path so we can import traderobots
+# api/ is at root. backend/ is at root. So ../backend is correct.
+sys.path.append(os.path.join(os.path.dirname(__file__), '../backend'))
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'traderobots.settings')
+
+application = get_wsgi_application()
